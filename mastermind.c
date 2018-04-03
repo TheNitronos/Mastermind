17af8d3bfabcdbce24b344776b2caa2eb3ed1489
Unknown <thenitronos@gmail.com>
2018-04-03 18:25:59 +0200

    I à la place de zu

--- mastermind.c
+++ mastermind.c
@@ -262,7 +262,7 @@ int main(void)
     do {
         printf("What size (2, 3, 4, 5)? ");
         fflush(stdout);
-        scanf("%zu", &size);
+        scanf("%I", &size);
     } while ((size != 2) && (size != 3) && (size != 4) && (size != 5));
 
     char strategy = '\0';
